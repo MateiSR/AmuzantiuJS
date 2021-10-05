048cd58524b10579e3af7b893c50f2b127dcc0cb
Matei <matei2radu11@gmail.com>
2021-10-06 00:49:28 +0300

    Fix purge command (delete call)

--- commands/utils/purge.js
+++ commands/utils/purge.js
@@ -9,7 +9,7 @@ module.exports = {
         if (!args[0]) args[0] = 10;
         if (isNaN(args[0]) || args[0] < 1 || args[0] > 100) return await message.reply("Please specify a number **between 1 and 100**.");
 
-        await message.channel.messages.fetch({ limit: args[0] }).then(messages => {
+        await message.channel.messages.fetch({ limit: parseInt(args[0]) + 1 }).then(messages => {
             message.channel.bulkDelete(messages);
         })
     }
