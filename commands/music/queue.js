d5fff74d6e4d70e97d37ce55a678239b17caa035
Matei <matei2radu11@gmail.com>
2021-10-07 15:53:25 +0300

    previous track

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -3,13 +3,23 @@ const { chunkArray, parseDuration } = require("../../utils/format.js");
 const { runCheck } = require("../../utils/decorators.js");
 const current = require("./current.js");
 
-const getQueuePage = function(queueSliced, pageNum) {
+const getQueuePage = function(player, pageNum) {
     // pageNum = index + 1
+    let queueSliced = chunkArray(player.queue, 10);
     let songLength;
     var formattedQueue = [];
     let c, c2, ret;
     var songNum = 0;
     let page = queueSliced[pageNum - 1];
+    // Add current track
+    if (pageNum == 1) {
+        let cTrack = player.queue.current;
+        let cTLength = parseDuration(cTrack.duration);
+        c = `--> ${cTrack.title.slice(0, 40)}`;
+        c2 = c + " ".repeat(48 - c.length) + cTLength;
+        formattedQueue.push(c);
+    }
+
     for (song of page) {
         songLength = parseDuration(song.duration);
         c = `${songNum + 1 + 10 * (pageNum - 1)}) ${song.title.slice(0, 40)}`;
@@ -34,7 +44,7 @@ module.exports = {
         let parsedPageNum = parseInt(args[0]);
         if (!args[0]) parsedPageNum = 1;
         if (parsedPageNum != NaN) {
-            if (parsedPageNum <= queueSliced.length && parsedPageNum >= 1) return await message.channel.send(getQueuePage(queueSliced, parsedPageNum));
+            if (parsedPageNum <= queueSliced.length && parsedPageNum >= 1) return await message.channel.send(getQueuePage(player, parsedPageNum));
         }
     }
 }
