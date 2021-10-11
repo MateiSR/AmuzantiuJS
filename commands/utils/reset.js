3fd09b274baceda26c2caf168009f50719eee18e
Matei <matei2radu11@gmail.com>
2021-10-11 11:24:40 +0300

    Reset via heroku API

--- commands/utils/reset.js
+++ commands/utils/reset.js
@@ -1,14 +1,18 @@
+const axios = require("axios");
+
 module.exports = {
     name: "reset",
     aliases: ["restart", "rs"],
-    description: "(admin) restarts bot",
+    description: "(admin) restarts bot via Heroku API",
     whitelist: ["240781589066285056"],
     async execute(message, args) {
-        console.log(">> Client reset called");
-        await message.reply("Resetting...");
-        await client.destroy();
-        await client.login(process.env.TOKEN);
-        await message.react("✅");
-        console.log(">> Client reset finished succesfully");
+        message.reply("Resetting...");
+        axios.delete("https://api.heroku.com/apps/mateisr-js/dynos/worker", {
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/vnd.heroku+json; version=3",
+                "Authorization": `Bearer ${process.env.HEROKU_AUTH}`
+            }
+        })
     }
 }
