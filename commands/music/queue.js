d751f09d0baf08b389fe10c79ea51117853b156d
Matei <matei2radu11@gmail.com>
2021-10-15 17:03:50 +0300

    Queue Formatting Update

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -7,8 +7,11 @@ const { MessageEmbed } = require("discord.js");
 const getQueuePage = function(player, pageNum) {
     // pageNum = index + 1
     let queueSliced = chunkArray(player.queue, 10);
+    let formattedQueue = [];
+    var songNum = 0;
     const toSend = new MessageEmbed()
-        .setTitle(`Queue for ${player.guild} | Page ${pageNum}/${queueSliced.length}`);
+        .setTitle(`Song Queue -  Page ${pageNum}/${queueSliced.length}`)
+        .setColor("#5865F2");
     let page = queueSliced[pageNum - 1];
 
     // Add current track
@@ -16,8 +19,21 @@ const getQueuePage = function(player, pageNum) {
         let cTrack = player.queue.current;
         let parsedPosition = parseDuration(player.position);
         let parsedDuration = parseDuration(cTrack.duration);
-        toSend.addField(`Now playing: [${cTrack.title}](${cTrack.uri})`, `Among`)
+
+        formattedQueue.push(`__Now Playing:__\n[${cTrack.title}](${cTrack.uri})\n${parsedPosition} / ${parsedDuration} [${cTrack.requester}]`);
     }
+    formattedQueue.push("__Up next__:");
+
+    for (track of page) {
+        let trackLength = parseDuration(track.duration);
+        formattedQueue.push(`\`${songNum + 1 + 10 * (pageNum - 1)}.\` [${track.title}](${track.uri}) | \`${trackLength}\` [${track.requester}]`);
+        songNum++;
+    }
+
+    formattedQueue.push(`**${player.queue.length} songs in queue | ${parseDuration(player.queue.duration)} total length**`);
+    formattedQueue.push(`**Loop:** ${player.trackRepeat} | **Queue Loop:** ${player.queueRepeat}`)
+
+    toSend.setDescription(formattedQueue.join("\n\n"));
 
     return toSend;
 };
